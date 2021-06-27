e42e14466cfce59efb1c28a930a021231ef6dea1
Amlan Guru <amlanguru152@gmail.com>
2021-06-27 14:42:54 +0530

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./header";
+import Info from "./info";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>ShapeAI Bootcamp</h1>
+      <p>All the required info</p>
+    </div>
+  );
+}
+
+export default Info;
